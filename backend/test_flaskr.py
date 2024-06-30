0c4397c5cdfa1c51489507256592057e32ce8f1c
tharunkumars <tharunkumars@gmail.com>
2024-06-30 23:21:17 +0530

    Initial Learnings 2

---
+++ backend/test_flaskr.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from backend.flaskr.models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.database_name = "trivia_test"
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        
+        self.app = create_app({
+            "SQLALCHEMY_DATABASE_URI": self.database_path
+        })
+
+        self.client = self.app.test_client
+
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
